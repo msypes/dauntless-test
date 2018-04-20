e3737632ab23dcd05389c437e3d7407dde483465
Michael A. Sypes <msypes@gmail.com>
2018-04-20 09:17:26 -0700

    Basic authentication thanks to Laravel's built in system.

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('properties/search', 'PropertyController@search');
-Route::resource('properties', 'PropertyController');
+Route::get('properties/search', 'PropertyController@search')->middleware('auth');
+Route::resource('properties', 'PropertyController')->middleware('auth');
 
-Route::get('bookingdates/search', 'BookingDateController@search');
-Route::resource('bookingdates', 'BookingDateController');
+Route::get('bookingdates/search', 'BookingDateController@search')->middleware('auth');
+Route::resource('bookingdates', 'BookingDateController')->middleware('auth');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
